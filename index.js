0c7e287f13fcc62e1610501c1997eced01283639
LD211 <93799505+LD211@users.noreply.github.com>
2024-01-04 15:25:09 -0500

    automated update.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const gid = (string) => { return document.getElementById(string) } // abstraction
-const gcl = (string) => { return document.querySelector(string) }
-const input = gid("input")
+const gid = (string) => {
+  return document.getElementById(string);
+}; // abstraction
+const gcl = (string) => {
+  return document.querySelector(string);
+};
+const input = gid("input");
 const output = gid("output");
 const cursor = gid("cursor");
 const prmpt = gcl(".prompt");
@@ -9,9 +13,37 @@ let id = 0;
 let activeId = false;
 
 const TEXTSPEED = 25;
-const introduction = "Lucas DePaola 2024``Type 'commands' for all listed commands."
+const introduction =
+  "Lucas DePaola 2024`The only reason you could be here is by googling my name, I won't judge.`Type 'commands' for all listed commands.";
 slowText(introduction);
 
+class File {
+  constructor(name, type, contents) {
+    this.name = name;
+    this.type = type;
+    this.contents = contents;
+  }
+}
+
+class Directory {
+  constructor(name, contents, prev) {
+    this.name = name;
+    this.contents = contents;
+    this.prev = prev;
+  }
+}
+
+function initFileSystem() {
+  const rootDir = new Directory("root", null, null);
+  rootDir.contents = [
+    new File("test.txt", "txt", "hello world"),
+    new File("script.js", "js", "console.log('hi')"),
+    new Directory("home", [
+      new File("secret.txt", "txt", "my github password is lucas100!77"),
+    ], null),
+  ];
+  return rootDir;
+}
 
 document.addEventListener("keydown", (key) => { // terminal listener
   updateCursor("input");
@@ -20,19 +52,67 @@ document.addEventListener("keydown", (key) => { // terminal listener
   if (key.key === "Enter") {
     interpretText(input.textContent);
     input.textContent = "";
+  } else if (key.key === "Escape") input.textContent = "";
+  else if (key.ctrlKey && key.key === "l") {
+    event.preventDefault();
+    input.textContent = "";
+  } else if (key.ctrlKey && key.key === "Backspace") ctrlBack();
+  else if (key.key === "Backspace") {
+    input.textContent = input.textContent.slice(0, -1);
   }
-  else if (key.key === "Escape") input.textContent = "";
-  else if (key.ctrlKey && key.key === "l") { event.preventDefault(); input.textContent = "" }
-  else if (key.ctrlKey && key.key === "Backspace") ctrlBack();
-  else if (key.key === "Backspace") input.textContent = input.textContent.slice(0, -1); updateCursor("input");
+  updateCursor("input");
+});
+
+window.addEventListener("resize", () => {
+  updateCursor(); // TODO: have the resize update work properly, event doesn't capture it
 });
 
 function interpretText(string) {
   string = string.trim();
   if (string === "test") slowText("this is a test");
   else if (string === "c" || string === "clear") clear();
+  else if (string === "commands") {
+    let commands = "c: clear screen`ls: list contents of the current directory";
+    commands += "`cd: change directory`cat: display contents of a file";
+    slowText(commands);
+  } else if (string === "ls") ls();
+  else if (string.split(" ")[0] === "cat") cat(string.split(" ")[1]);
+  else if (string.split(" ")[0] === "cd") cd(string.split(" ")[1]);
   else slowText("unknown command");
   return "";
+}
+
+function ls() {
+  let string = "";
+  for (element of currentDir.contents) {
+    string += element.name + "`";
+  }
+  slowText(string);
+}
+
+function cat(fileName) {
+  for (const element of currentDir.contents) {
+    if (element.name === fileName) {
+      slowText(element.contents);
+    }
+  }
+}
+
+function cd(dirName) {
+  if (dirName === "..") {
+    currentDir = currentDir.prev;
+    return;
+  }
+  for (const element of currentDir.contents) {
+    if (element instanceof Directory && element.name === dirName) {
+      element.prev = currentDir;
+      currentDir = element;
+      return;
+    }
+  }
+  slowText(
+    "Directory not found, run 'ls' to find the current files/directories.",
+  );
 }
 
 function updateCursor(boundelement) {
@@ -43,17 +123,30 @@ function updateCursor(boundelement) {
   cursor.style.height = coords.height + "px";
 }
 
-const clear = () => { output.innerHTML = "" }
+const clear = () => {
+  if (activeId) {
+    clearInterval(id);
+    activeId = false;
+  }
+  output.innerHTML = "";
+};
 
 function slowText(text) {
   if (activeId) return;
   activeId = true;
   let i = 0;
   id = setInterval(() => {
-    if (text[i] === "`") { output.innerHTML += "<br>"; i++ }
-    else { output.innerHTML += text[i++]; }
+    if (text[i] === "`") {
+      output.innerHTML += "<br>";
+      i++;
+    } else output.innerHTML += text[i++];
     updateCursor("input");
-    if (i > text.length - 1) { output.innerHTML += "<br><br>"; updateCursor("input"); clearInterval(id); activeId = false }
+    if (i > text.length - 1) {
+      output.innerHTML += "<br><br>";
+      updateCursor("input");
+      clearInterval(id);
+      activeId = false;
+    }
   }, TEXTSPEED);
 }
 
@@ -69,3 +162,5 @@ function ctrlBack() {
   }
   input.textContent = "";
 }
+const root = initFileSystem();
+let currentDir = root;
